8bea179e4981f9b0e7b111a1f785a8e3d90b791d
mintc <mintc@192.168.219.105>
2024-04-22 22:04:52 +0900

    ddddd

--- 121212/study/One.java
+++ 121212/study/One.java
@@ -1,8 +1,25 @@
 package study;
 
+import java.util.Arrays;
+import java.util.Collections;
+
 public class One {
 	/*
 	 * 일단 이클립스와 연동부터 해결 좀 합시다... ㅠ
 	 * 
 	 * */
-}
+	    public String solution(String s) {
+	        Character[] chars = new Character[s.length()];
+	        for (int i = 0; i < s.length(); i++) {
+	            chars[i] = s.charAt(i);
+	        }
+	        // 배열을 내림차순으로 정렬
+	        Arrays.sort(chars, Collections.reverseOrder());
+	        // 정렬된 배열을 문자열로 변환
+	        StringBuilder sb = new StringBuilder(chars.length);
+	        for (Character c : chars) {
+	            sb.append(c);
+	        }
+	        return sb.toString();
+	    }
+	}
